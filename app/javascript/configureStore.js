fe7b51fb29afee37f6b97db9fe080674fec3d6b9
Sylvestre <pasytchangwa@gmail.com>
2021-12-08 16:21:52 +0200

    Display the random greeting in your Greeting component.

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,12 +1,15 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './redux/greetings';
 
 export default function configureStore() {
   const store = createStore(
     reducer,
-    composeWithDevTools(applyMiddleware(thunk)),
+    composeWithDevTools(applyMiddleware(
+      thunk,
+    )),
   );
   return store;
 }
