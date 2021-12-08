4e4de3b4bf6c59ecbc707a16dcb49e9c0ff716cb
Sylvestre <pasytchangwa@gmail.com>
2021-12-08 17:15:06 +0200

    Fix linters

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './redux/greetings';
 
