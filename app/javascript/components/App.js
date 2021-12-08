40a31b2fc18b16b902558def19600edda0f663c4
Sylvestre <pasytchangwa@gmail.com>
2021-12-08 15:07:33 +0200

    Create the App component with react-router and render it in your static view

---
+++ app/javascript/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
