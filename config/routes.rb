4af5c3ade433fee000ea411651876380a7ff6d86
Sylvestre <pasytchangwa@gmail.com>
2021-12-08 14:55:48 +0200

    Create a static view that will be the root of your app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'statics/index'
   namespace :v1, defaults: {format: JSON} do
     get 'greetings', to: 'greetings#index'
   end
+
+  root 'static#index'
 end
